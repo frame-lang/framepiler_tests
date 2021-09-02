9db1c20d80f3ee0c54a363cc8f1da947fef29a41
gaurav-ongraph <gaurav.saxena@ongraphmail.com>
2021-09-02 18:21:38 +0530

    Add prod logs to console

--- src/logger/prod-logger.js
+++ src/logger/prod-logger.js
@@ -16,6 +16,7 @@ const buildProdLogger = () => {
     
     const logConfiguration = {
         'transports': [
+            new winston.transports.Console(),
             new winston.transports.File({
                 filename: logPath
             })
